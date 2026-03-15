150455e2d322a6a9680b1328046fbaef772dc11f
maherbaldi969 <maherbaaldi@gmail.com>
2026-03-15 18:01:39 +0100

    maher

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProjectRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +10,15 @@ use Symfony\Component\Routing\Annotation\Route;
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(ProjectRepository $projectRepository): Response
     {
-        return $this->render('home/index.html.twig');
+        $projects = $projectRepository->findBy(
+            ['isPublished' => true],
+            ['createdAt' => 'DESC']
+        );
+
+        return $this->render('home/index.html.twig', [
+            'projects_db' => $projects
+        ]);
     }
 }
